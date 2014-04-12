cfe58a3a96114883de9ad77c8a03eb33efd89247
Coty Beasley <design@beasleycreative.com>
2014-04-12 12:52:08 -0500

    Additions to user profile.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('neroAppApp', [
+angular.module('neroApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('neroAppApp')
+angular.module('neroApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
