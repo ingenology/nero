ff148d94c29280d1167b1a84ed3d0724e052d0bf
Coty Beasley <design@beasleycreative.com>
2014-04-12 09:44:43 -0500

    Clobbered to be an angular app now.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('neroAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
