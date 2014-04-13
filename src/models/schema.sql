8e1742b28b45e2cb707d8059e38eed1d3bc82143
Cuong Cu <csc823@gmail.com>
2014-04-12 20:13:15 -0500

    Schema changes

--- src/models/schema.sql
+++ src/models/schema.sql
@@ -1,34 +1,35 @@
--- User table
+DROP TABLE IF EXISTS journals;
 DROP TABLE IF EXISTS users;
 
+-- User table
 CREATE TABLE users (
-  id INTEGER NOT NULL AUTO_INCREMENT,
+  id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
   password BLOB NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
 
   CONSTRAINT id_pk PRIMARY KEY (id)
 ) ENGINE = INNODB;
 
-
-
 -- Journal table
-DROP TABLE IF EXISTS journals;
-
 CREATE TABLE journals (
-  id INTEGER NOT NULL AUTO_INCREMENT,
+  id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
   user_id INTEGER NOT NULL,
   title VARCHAR(255) NOT NULL,
-  latitude FLOAT NOT NULL,
-  longitude FLOAT NOT NULL,
+  latitude FLOAT(20) NOT NULL,
+  longitude FLOAT(20) NOT NULL,
   create_date long NOT NULL,
-  notes VARCHAR(255) NOT NULL,
-  hashtags VARCHAR(255) NOT NULL,
-  altitude FLOAT NOT NULL,
-  azimuth FLOAT NOT NULL,
-  gamma FLOAT NOT NULL,
+  notes VARCHAR(255),
+  hashtags VARCHAR(255),
+  altitude FLOAT(20),
+  azimuth FLOAT(20),
+  gamma FLOAT(20),
 
-  CONSTRAINT id_pk PRIMARY KEY (id),
-  CONSTRAINT user_id_fk FOREIGN KEY journals(user_id) REFERENCES users(id)
+  CONSTRAINT id_pk PRIMARY KEY (id)
 ) ENGINE = INNODB;
+
+ALTER TABLE journals
+ADD CONSTRAINT user_id_fk
+FOREIGN KEY  journals(user_id)
+REFERENCES users(id);
