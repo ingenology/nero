57f91ae463762ebf96bdf06a48abe0e1a0b84b4e
Coty Beasley <coty@cotybeasley.name>
2014-04-19 20:29:26 -0500

    Fixed some controller stuff and added JSON populated data.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/* --------------------------------------------------------------------------
+ Angular Stuff
+ -------------------------------------------------------------------------- */
+
 angular.module('neroApp', [
   'ngCookies',
   'ngResource',
@@ -10,7 +14,7 @@ angular.module('neroApp', [
         $routeProvider
             .when('/', {
                 templateUrl: 'views/entryList.html',
-                controller: 'MainCtrl'
+                controller: 'EntryCtrl'
             })
             .when('/fieldGuide', {
                 templateUrl: 'views/fieldGuide.html',
@@ -32,3 +36,42 @@ angular.module('neroApp', [
                 redirectTo: '/'
             });
     });
+
+/* --------------------------------------------------------------------------
+ Phone Capture Data
+ -------------------------------------------------------------------------- */
+
+function getLocation() {
+    $('.posDataReadout .data').hide();
+    $('.posDataReadout .data *').html();
+    $('.readoutWait').show();
+    $('.posDataReadout').show();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        readout.html("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    $('.data-lat').html('<div>Latitude: <span>' + position.coords.latitude + ' deg</span></div>');
+    $('.data-long').html('<div>Longitude: <span>' + position.coords.longitude + ' deg</span></div>');
+    $('.data-azi').html('<div>Azimuth: <span>' + 89 + ' deg</span></div>');
+    $('.data-alt').html('<div>Altitude: <span>' + 65 + ' deg</span></div>');
+    $('.readoutWait').hide();
+    $('.posDataReadout .data').fadeIn();
+    $('.capturePosData').html('Recapture Data');
+
+}
+
+/* --------------------------------------------------------------------------
+ Navigation Interaction
+ -------------------------------------------------------------------------- */
+
+// TODO Add Activate Class Logic
+$('.nav-primary li > a').click(
+    function() {
+        $('.nav-primary li > a').removeClass('active');
+        $(this).addClass('active');
+    }
+);
