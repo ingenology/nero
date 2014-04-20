3feda3a4d2ba2cb6fe91e95f616687d30ec904dc
Coty Beasley <coty@cotybeasley.name>
2014-04-20 12:58:41 -0500

    Added a couple things to make jshint stop being upset.

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,8 @@ module.exports = function (grunt) {
       },
       all: [
         'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js'
+        '<%= yeoman.app %>/scripts/{,*/}*.js',
+        '!assets/js/plugins/jquery.gomap-1.3.2.js'
       ],
       test: {
         options: {
